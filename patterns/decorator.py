15c49b949643901ab22a8cb6385fdeee3a2af98d
onionmccabbage <onionmccabbage@live.com>
2025-04-07 10:25:09 +0100

    decorator pattern example

--- patterns/decorator.py
+++ patterns/decorator.py
@@ -1,6 +1,20 @@
 # The decorator pattern is used widely within Python
 # Anything that begins with '@' is a Python decorator
 
+# we may write our own decorator function
+def showArgs(func): # whichever function we decorate will be passed in as an argument
+    '''This function may be used as a decorator for any other function
+    It will reveal all the positional arguemtns and keyword argument'''
+    def newFunc(*args, **kwargs): # * and ** will unpack the collections
+        '''expose the args/kwargs'''
+        print(f'''Running a function called {func.__name__}
+The positional arguments are {args}
+The keyword arguments are {kwargs}''')
+        # remember to call the original function
+        return func(*args, **kwargs)
+    return newFunc # we do not invoke the new function
+
+@showArgs # we apply our decorator like this
 def squares(m ,n):
     '''return a list of the square integers between m and n'''
     s = []
