bb39e9ca4ee3b1318d49861230c189c6fca5b097
onionmccabbage <onionmccabbage@live.com>
2025-04-08 11:03:11 +0100

    using state

--- patterns/using_state/_pc_state.py
+++ patterns/using_state/_pc_state.py
@@ -5,5 +5,6 @@ class ComputerState():
         if new_state.name in self.allowed:
             print(f'Current state {self} switching to {new_state}')
             self.__class__ = new_state
+            # self.current_state = new_state
         else:
             print(f'Current state {self} cannot switch to {new_state}')
