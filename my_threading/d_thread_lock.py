0cf8c3f45d009a49c4dcb9e32030928419e7a8c6
onionmccabbage <onionmccabbage@live.com>
2025-04-08 14:24:22 +0100

    thread-safe with locks

--- my_threading/d_thread_lock.py
+++ my_threading/d_thread_lock.py
@@ -11,22 +11,34 @@ ticketsAvailable = 100
 class TicketSeller(threading.Thread):
     '''imitate random time between sales'''
     ticketsSold = 0 # class member
-    def __init__(self):
-        threading.Thread.__init__()
+    def __init__(self, lock):
+        threading.Thread.__init__(self)
+        self.lock = lock # each of our threads has access to the SAME lock
         print(f'Ticket seller starts selling tickets')
     def run(self):
         global ticketsAvailable   # or access a db, a file, an API
         running = True
         while running:
             self.randomDelay()
+            self.lock.acquire() # exclusively access shared resources
             if ticketsAvailable <=0:
                 running = False # stop running!
             else:
                 ticketsAvailable -= 1
                 self.ticketsSold += 1
                 print(f'Sold a ticket {ticketsAvailable} remaining')
+            self.lock.release() # mke global assets avaialble to other threads
         print(f'Sold {self.ticketsSold}')
-    def randomDelay():
+    def randomDelay(self):
         time.sleep(random.randint(0,4)/4) # 0, 0.25, 0.5, 0.75
 
-
+if __name__ == '__main__':
+    sellers_list = []
+    # we may use a thread lock to make threads safe (nique access to global assets)
+    lock = threading.Lock() # we have an instance of e lock
+    for _ in range(0, 4):
+        seller = TicketSeller(lock)
+        sellers_list.append(seller)
+        seller.start() # each thread is invoked
+    for _ in sellers_list:
+        _.join()
