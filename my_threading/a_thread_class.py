2ccf3b7cfc1559c481bb291976b9cd3f57703de5
onionmccabbage <onionmccabbage@live.com>
2025-04-08 13:42:45 +0100

    simple thread class with __call__

--- my_threading/a_thread_class.py
+++ my_threading/a_thread_class.py
@@ -15,7 +15,16 @@ def main():
     c1 = MyClass()
     # we need a thread instance
     t1 = Thread(target=c1, args=(1,)) # NB a one-member tuple MUST have trailing comma
-    t1.start()
+    t2 = Thread(target=c1, args=(2,))
+    # we can run more than one additional thread concurrently
+    t1.start() # invoke the __call__ method of the class instance
+    t2.start()
+    t1.join() # pause the main thread until t1 (re)joins
+    t2.join()
+    print('main thread...')
+
+    # it's a good idea to tidy up
+    # check about stopping redundant threads
 
 
 if __name__ == '__main__':
