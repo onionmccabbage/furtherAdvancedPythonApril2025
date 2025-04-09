a67a8016af02b9518fb028f2083bc7af48f70298
onionmccabbage <onionmccabbage@live.com>
2025-04-09 11:46:25 +0100

    simple multiprocessing example

--- my_processing/a_proc.py
+++ my_processing/a_proc.py
@@ -1,13 +1,31 @@
 import multiprocessing
 import os
+import time
+import timeit
 
 def myProc(n):
     '''run this on a separate process'''
     print(f'{n} Running in a separate process {os.getpid()}')
+    time.sleep(2)
 
 if __name__ == '__main__':
+    start = timeit.default_timer()
     print(f'The main process is running on {os.getpid()}')
-    pA = multiprocessing.Process(target=myProc, args=(1,))
-    pB = multiprocessing.Process(target=myProc, args=(2,))
-    pA.start()
-    pB.start()
+    # a list to hold our process instances
+    pl=[]
+    # a loop (or comprehension) to start several processes
+    for p in range(64):
+        proc = multiprocessing.Process(target=myProc, args=(p,))
+        proc.start()
+        pl.append(proc)
+    # a loop to join
+    d = [p.join for p in pl]
+    # pA = multiprocessing.Process(target=myProc, args=(1,))
+    # pB = multiprocessing.Process(target=myProc, args=(2,))
+    # pA.start()
+    # pB.start()
+    # # the main proces carries on unless we tell it to wait for the other proceses
+    # pA.join()
+    # pB.join()
+    end = timeit.default_timer()
+    print(f'Total time: {end-start}')
