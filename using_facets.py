416be6ddd5d0b6965b9d955ad664ca864c47ac37
onionmccabbage <onionmccabbage@live.com>
2025-04-09 15:44:54 +0100

    limits of python

---
+++ using_facets.py
@@ -0,0 +1,9 @@
+import sys
+import os
+
+print(sys.maxsize)
+print(os.getpid())
+print(sys.float_info)
+
+# ALL computers are incapable of handling real numbers
+# they can only handle than vanishingly small subset of numbers which can be represented in 64 bits
