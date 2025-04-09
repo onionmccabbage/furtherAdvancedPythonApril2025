dcdfd65cac10a60fab4b05c1bbadb659baa803fa
onionmccabbage <onionmccabbage@live.com>
2025-04-09 13:42:17 +0100

    text for review 3

--- my_profiling/fib.py
+++ my_profiling/fib.py
@@ -1,10 +1,12 @@
 import timeit
 from functools import reduce
-
+from memory_profiler import profile
 # we may atain a profile of any code using cProfile
 # python -m cProfile -o profile_output fib.py
 
 
+# we can only use @profile to decorate functions and classes
+@profile
 def fib1(n):
     '''here is a low performance fibonacci function'''
     if n in (0,1):
@@ -21,8 +23,11 @@ def fib2(n):
         sequence += (reduce( lambda a,b: a+b, sequence[-2:] ),)
     return sequence[-1] # return the last member of the sequence
 
-if __name__ == '__main__':
+def main():
     start = timeit.default_timer()
     print(fib1(28)) # 0.14 sec for fib1, 0.00013 for fib2
     end=timeit.default_timer()
     print(f'total time: {end-start}')
+
+if __name__ == '__main__':
+    main()
