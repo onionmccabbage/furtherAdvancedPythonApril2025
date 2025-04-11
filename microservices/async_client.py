45fa7c6933ed800e595062b7efa95cfb6a9c9e60
onionmccabbage <onionmccabbage@live.com>
2025-04-11 10:59:23 +0100

    simple byte writer

--- microservices/async_client.py
+++ microservices/async_client.py
@@ -9,5 +9,28 @@ async def my_client(message):
     await writer.drain() # asynchronously wait until the writer has completed
     writer.close()
 
+# a function to create encoded bytes
+# The majority of computer files are not human readable - they are byte files
+def makeBytes(values):
+    # lets make some bytes
+    b = bytes( values ) 
+    print(b) # print will nicely format the bytes as text
+    return b
+
+def writeBytes(b):
+    # persist these bytes into a file
+    try:
+        fout = open('bfile', 'ab') # 'w' will (over)write, 'b' for bytes
+        fout.write(b)
+        fout.close()
+    except FileExistsError as err:
+        print(f'File already exists {err}')
+    except Exception as err:
+        print(f'something went wrong {err}')
+
+
 if __name__ == '__main__':
-    asyncio.run(my_client('is it coffee yet'))
+    # asyncio.run(my_client('is it coffee yet'))
+    values = range(0,256)
+    b = makeBytes(values)
+    writeBytes(b)
