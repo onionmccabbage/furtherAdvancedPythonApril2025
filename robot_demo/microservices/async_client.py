3e97e0bb45b26bd730d9995936a736c0e1d93704
onionmccabbage <onionmccabbage@live.com>
2025-04-11 10:27:06 +0100

    simple async client

---
+++ robot_demo/microservices/async_client.py
@@ -0,0 +1,13 @@
+import asyncio
+
+async def my_client(message):
+    '''a simple asynchronous client
+    send a message to the async server'''
+    reader, writer = await asyncio.open_connection('localhost', 8889)
+    print(f'Senfing {message}')
+    writer.write(message.encode()) # all communication over http needs to be encoded
+    await writer.drain() # asynchronously wait until the writer has completed
+    writer.close()
+
+if __name__ == '__main__':
+    asyncio.run(my_client('is it coffee yet'))
