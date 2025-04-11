896f36560e7d0f3bf0d3f0c860440d2b3ace75ee
onionmccabbage <onionmccabbage@live.com>
2025-04-11 11:47:12 +0100

    simple Flask server

--- FlaskMicroservice/flask_server.py
+++ FlaskMicroservice/flask_server.py
@@ -11,7 +11,28 @@ def main():
     @app.route('/') #this is the top level of our server
     def root():
         return 'welcome'
-    app.run() # start the server
+    @app.route('/hello')
+    def hello():
+        return '<h3>Hello</h3>'
+    # multiple routes to the same content
+    @app.route('/info')
+    @app.route('/about')
+    @app.route('/stuff')
+    def generic():
+        '''any of the three routes will end up here'''
+        return '<p>Information about stuff</p>'
+
+    # using URL parameters
+    @app.route('/greet/<person>') # <> mean there will be a parameter
+    def greet(person='Friend'):
+        if person:
+            return f'<h3>Welcome {person}</h3>'
+        else:
+            return 'Welcome to Flask Server'
+
+    # using HTML templates
+
+    app.run(debug=True) # start the server as a listening server (restart on change)
 
 
 if __name__ == '__main__':
